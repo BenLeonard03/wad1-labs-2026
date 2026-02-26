d7bf7b76a3cf56b02e9300fc65b79314a5f82a88
Ben Leonard <20101994@mail.wit.ie>
2026-02-26 17:11:03 +0000

    Add Models and Views

---
+++ controllers/playlist.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import logger from '../utils/logger.js';
+
+const playlist = {
+  createView(request, response) {
+    const playlistId = request.params.id;
+    logger.debug(`Playlist id = ${playlistId}`);
+    
+    const viewData = {
+      title: 'Playlist',
+      singlePlaylist: playlistStore.getPlaylist(playlistId)
+    };
+    response.render('playlist', viewData);
+  },
+};
+
+export default playlist;

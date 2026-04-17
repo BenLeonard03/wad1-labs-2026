b80c8e187127351c824bbd48d4036766de538ca7
Ben Leonard <20101994@mail.wit.ie>
2026-04-17 09:21:56 +0100

    Added max songs statistics

--- routes.js
+++ routes.js
@@ -8,6 +8,7 @@ import start from './controllers/start.js';
 import dashboard from './controllers/dashboard.js';
 import about from './controllers/about.js';
 import playlist from './controllers/playlist.js';
+import stats from './controllers/stats.js';
 
 
 router.get('/', start.createView);
@@ -27,6 +28,7 @@ router.get('/error', (request, response) => response.status(404).end('Page not f
 
 router.post('/playlist/:id/updatesong/:songid', playlist.updateSong);
 
+router.get('/stats', stats.createView);
 
 export default router;
 

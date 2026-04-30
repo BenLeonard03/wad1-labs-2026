bf2b3963a0bc73405b5d93e6f18de3f224ae6217
Ben Leonard <20101994@mail.wit.ie>
2026-04-30 20:53:30 +0100

    User authentication added

--- controllers/about.js
+++ controllers/about.js
@@ -2,19 +2,25 @@
 
 import logger from "../utils/logger.js";
 import employeeList from "../models/employee-list.js";   // your model
+import accounts from './accounts.js';
 
 const about = {
+  
   createView(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
     logger.info("About page loading!");
+    
+    if (loggedInUser) {
+      const viewData = {
+        title: 'About the Playlist App',
+        fullname: loggedInUser.firstName + ' ' + loggedInUser.lastName,
+        employees: empStore.getEmployees(),
+      };
+      response.render('about', viewData);
+    }
+    else response.redirect('/');    
+},
 
-    const viewData = {
-      title: "About",
-      employees: employeeList.getEmpInfo()   // <-- now returns an array
-    };
-
-    logger.info(viewData.employees);
-    response.render("about", viewData);
-  },
 };
 
 export default about;
--- controllers/start.js
+++ controllers/start.js
@@ -2,22 +2,25 @@
 
 import logger from "../utils/logger.js";
 import appStore from "../models/app-store.js";
+import accounts from './accounts.js';
 
 const start = {
+  
   createView(request, response) {
-
+    const loggedInUser = accounts.getCurrentUser(request);
     logger.info("Start page loading!");
     
-    const viewData = {
+    if (loggedInUser) {
+      const viewData = {
+        title: "Welcome to the Playlist app!",
+        info: appStore.getAppInfo(),
+        fullname: loggedInUser.firstName + ' ' + loggedInUser.lastName,
+      };
+      response.render('start', viewData);
+    }
+    else response.redirect('/');    
+},
 
-      title: "Welcome to the Playlist App!",
-      info: appStore.getAppInfo()
-
-     };
-
-     response.render('start', viewData);
-      
-  },
 };
 
 export default start;
